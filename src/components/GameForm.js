d6eaa40c7bf5bcbbbcad9c35a87b5254245129bc
Christopher Karras <ckarras11@gmail.com>
2017-11-19 21:14:59 -0500

    Created a game form component to handle the user input

---
+++ src/components/GameForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './GameForm.css';
+
+export default function GameForm(props){
+
+  return (
+    <section className='game-form-container'>
+        <form className='game-form'>
+            <input value={props.value} onChange={props.onChange} type='number' placeholder='Enter your Guess' />
+            <button onClick={props.onClick} className='submit' disabled={props.isEnabled} >Submit</button>
+        </form>
+        <p>Guess: {props.guessNumber}</p>
+    </section>
+  )
+}
